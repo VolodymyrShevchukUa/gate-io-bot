167058f70186d9127922e1ce62a449b2517774b8
volodymyr <volodymyr.shevchuk@botscrew.com>
2024-09-17 18:39:42 +0300

    mega release

--- src/main/java/org/example/gateiobot/InterestRate.java
+++ src/main/java/org/example/gateiobot/InterestRate.java
@@ -1,14 +1,45 @@
 package org.example.gateiobot;
 
-public record InterestRate(String asset,
-                           String borrowedAmount,
-                           String borrowedAmountUsdt,
-                           String borrowAvailable,
-                           String borrowAvailableUsdt,
-                           String interestRateHour,
-                           String interestRateYear,
-                           String cryptoLoanFixedRateFor7DayHour,
-                           String cryptoLoanFixedRateFor7DayYear,
-                           String cryptoLoanFixedRateFor30DayHour,
-                           String cryptoLoanFixedRateFor30DayYear) {
+import java.util.Objects;
+
+public record InterestRate(String asset, String borrowedAmount, String borrowedAmountUsdt, String borrowAvailable,
+                           String borrowAvailableUsdt, String interestRateHour, String interestRateYear,
+                           String cryptoLoanFixedRateFor7DayHour, String cryptoLoanFixedRateFor7DayYear,
+                           String cryptoLoanFixedRateFor30DayHour, String cryptoLoanFixedRateFor30DayYear) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InterestRate that = (InterestRate) o;
+        return Objects.equals(interestRateHour, that.interestRateHour) &&
+                Objects.equals(interestRateYear, that.interestRateYear) &&
+                Objects.equals(cryptoLoanFixedRateFor7DayHour, that.cryptoLoanFixedRateFor7DayHour) &&
+                Objects.equals(cryptoLoanFixedRateFor7DayYear, that.cryptoLoanFixedRateFor7DayYear) &&
+                Objects.equals(cryptoLoanFixedRateFor30DayHour, that.cryptoLoanFixedRateFor30DayHour) &&
+                Objects.equals(cryptoLoanFixedRateFor30DayYear, that.cryptoLoanFixedRateFor30DayYear);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(interestRateHour, interestRateYear, cryptoLoanFixedRateFor7DayHour,
+                cryptoLoanFixedRateFor7DayYear, cryptoLoanFixedRateFor30DayHour, cryptoLoanFixedRateFor30DayYear);
+    }
+
+    @Override
+    public String toString() {
+        return "InterestRate{" +
+                "asset='" + asset + '\'' +
+                ", borrowedAmount='" + borrowedAmount + '\'' +
+                ", borrowedAmountUsdt='" + borrowedAmountUsdt + '\'' +
+                ", borrowAvailable='" + borrowAvailable + '\'' +
+                ", borrowAvailableUsdt='" + borrowAvailableUsdt + '\'' +
+                ", interestRateHour='" + interestRateHour + '\'' +
+                ", interestRateYear='" + interestRateYear + '\'' +
+                ", cryptoLoanFixedRateFor7DayHour='" + cryptoLoanFixedRateFor7DayHour + '\'' +
+                ", cryptoLoanFixedRateFor7DayYear='" + cryptoLoanFixedRateFor7DayYear + '\'' +
+                ", cryptoLoanFixedRateFor30DayHour='" + cryptoLoanFixedRateFor30DayHour + '\'' +
+                ", cryptoLoanFixedRateFor30DayYear='" + cryptoLoanFixedRateFor30DayYear + '\'' +
+                '}';
+    }
 }
